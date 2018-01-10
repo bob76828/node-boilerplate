1dce6a942db63106cec2864278456366c699aa5a
BobLin <bob76828@gmail.com>
2018-01-10 20:38:40 +0800

    Init project

---
+++ gulp-tasks/build/index.js
@@ -0,0 +1,34 @@
+import gulp from 'gulp';
+import runSequence from 'run-sequence';
+import gulpHelp from 'gulp-help';
+import del from 'del';
+import gulpUtil from 'gulp-util';
+import babel from 'gulp-babel';
+
+gulpHelp(gulp);
+
+const folderName = 'lib';
+const config = {
+  jsPath: [
+    `${folderName}/**/*.js`,
+    `!${folderName}/**/__test__/*.js`,
+  ],
+};
+
+gulp.task('babel', false, () => {
+  gulp.src(config.jsPath)
+    .pipe(babel())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', false, () => {
+  del.sync(['build']);
+  gulpUtil.log('build cleaned');
+});
+
+gulp.task('build', 'Production build', ['clean'], (callback) => {
+  runSequence(
+    ['babel'],
+    callback,
+  );
+});
